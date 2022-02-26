e511930040db911a722fe0495c377cc98214fb79
Miguel Arconada <100458947@alumnos.uc3m.es>
2022-02-26 08:44:19 +0100

    Version of the code applied to stereo audios

--- library.py
+++ library.py
@@ -2,8 +2,10 @@ import librosa
 from librosa.display import specshow
 import numpy as np
 import soundfile as sf
+from pydub import AudioSegment
+import os
 
-REDUNDANCY = 4
+REDUNDANCY = 2
 PADDINGWIDTH = 15
 BYTESFORLENGTHOFFILENAME = 1
 BYTESFORLENGTHOFSECRET = 2
@@ -51,15 +53,18 @@ def bits2File(bits, filename):
       byte = n.to_bytes(1, byteorder="big")
       f.write(byte)
 
-def length2Bits(length, nbytes): # 4 bytes
-  bits = [0,]*8*nbytes
-  n = length
-  for i in range(8*nbytes):
-    bits[8*nbytes-1-i] = n%2
-    n = n//2
-  return bits
+def length2Bits(length, nbytes):
+    # print(length)
+    bits = [0,]*8*nbytes
+    n = length
+    for i in range(8*nbytes):
+        bits[8*nbytes-1-i] = n%2
+        n = n//2
+    # print(bits)
+    return bits
 
 def bits2Length(bits, nbytes):
+    # print(bits)
     n = 0
     for i in range(nbytes):
         n_ = bits2Int(bits[i*8:(i+1)*8])
@@ -73,7 +78,6 @@ def filename2Bits(filename):
     for byte in filename.encode('utf-8'):
         newbits = int2Bits(byte)
         bits = bits + newbits
-
     return bits
 
 def bits2Filename(bits):
@@ -106,16 +110,29 @@ def expandBits(bits):
         result += [bit,] * REDUNDANCY
     return result
 
-def unexpandBits(bits):
-    # secretBits = [1 if b >= 0.5 else 0 for b in bits]
-    result = []
-    for i in range(0, len(bits) - len(bits)%REDUNDANCY, REDUNDANCY):
-        mean = sum([bits[j] for j in range(i, i + REDUNDANCY)]) / REDUNDANCY
+def invertBits(bits):
+    return [1-b for b in bits[::-1]]
 
-        if mean >= 0.5:
-            bit = 1
+def checkAndUnexpandBits(bits):
+    # bits[1] are already inverted and modified
+    result = []
+    # print(bits[0][:16])
+    # print(bits[1][:16])
+    for i in range(0, len(bits[0]) - len(bits[0])%REDUNDANCY, REDUNDANCY):
+        mean1 = sum([bits[0][j] for j in range(i, i + REDUNDANCY)]) / REDUNDANCY
+        mean2 = sum([bits[1][j] for j in range(i, i + REDUNDANCY)]) / REDUNDANCY
+
+        if abs(mean1-0.5) > abs(mean2-0.5):
+            if mean1 > 0.5:
+                bit = 1
+            else:
+                bit = 0
         else:
-            bit = 0
+            if mean2 > 0.5:
+                bit = 1
+            else:
+                bit = 0
+
         result.append(bit)
 
     return result
@@ -125,13 +142,17 @@ def unexpandBits(bits):
 
 ######
 
-def encode(audioFilename, secretFilename, outputFilename):
+def encode(audioFilename, secretFilename, outputFilename, frequencies):
     try:
-        sig, fs = librosa.core.load(audioFilename)#, sr=8000)
+        sig, fs = librosa.core.load(audioFilename, mono=False)#, sr=8000)
     except:
         print("Error opening file")
         return
     print(f"Sampling rate is {fs}")
+
+    if sig.shape[0] != 2:
+        print("Necessary to use a stereo file")
+        exit()
 
     abs_spectrogram = signal2Spectrogram(sig)
     print("Spectrogram created")
@@ -170,15 +191,22 @@ def encode(audioFilename, secretFilename, outputFilename):
     # print(secretBits[0:16])REDUNDANCY * 4 * 8 + lengthOfFilename * REDUNDANCY + REDUNDANCY * 4 * 8
     # expandedSecretBits += [0,] * (abs_spectrogram.shape[1] - len(expandedSecretBits))
 
-    for i in range(-PADDINGWIDTH, PADDINGWIDTH):
-        abs_spectrogram[1000+i][:len(expandedSecretBits)] = expandedSecretBits
+    invertedExpandedSecretBits = invertBits(expandedSecretBits)
 
-    print("Bits written to all frequencies")
+    print("Writting bits to frequencies...", end='\r')
+    for fr in frequencies: # TODO: Decide whether to write the same to all or append the message
+        for i in range(-PADDINGWIDTH, PADDINGWIDTH):
+            abs_spectrogram[0][fr+i][:len(expandedSecretBits)] = expandedSecretBits
+            abs_spectrogram[1][fr+i][-len(expandedSecretBits):] = invertedExpandedSecretBits
+
+    print("Writting bits to frequencies... OK")
 
     # showSpectrogram(abs_spectrogram, fs)
 
     print("Converting spectrogram to audio signal... ", end='\r')
-    audio_signal = spectrogram2AudioSignal(abs_spectrogram)
+    audio_signal1 = spectrogram2AudioSignal(abs_spectrogram[0])
+    print("Converting spectrogram to audio signal... 1/2", end='\r')
+    audio_signal2 = spectrogram2AudioSignal(abs_spectrogram[1])
     print("Converting spectrogram to audio signal... OK!")
 
     # # print(sig, sig.shape)
@@ -186,29 +214,51 @@ def encode(audioFilename, secretFilename, outputFilename):
     # # print(audio_signal, audio_signal.shape)
 
     # write output
-    sf.write(outputFilename, audio_signal, fs, 'PCM_24')
+    sf.write("auxfile1.wav", audio_signal1, fs, 'PCM_24')
+    sf.write("auxfile2.wav", audio_signal2, fs, 'PCM_24')
+
+    left_channel = AudioSegment.from_wav("auxfile1.wav")
+    right_channel = AudioSegment.from_wav("auxfile2.wav")
+    stereo_sound = AudioSegment.from_mono_audiosegments(left_channel, right_channel)
+
+    os.remove("auxfile1.wav")
+    os.remove("auxfile2.wav")
+
+    stereo_sound.export(out_f = outputFilename, format = "wav")
+
     print(f"Audio signal written to {outputFilename}")
 
 def decode(audioFilename):
     try:
-        sig, fs = librosa.core.load(audioFilename)#, sr=8000)
+        sig, fs = librosa.core.load(audioFilename, mono=False)#, sr=8000)
     except:
         print("Error opening file")
         return
     print(f"Sampling rate is {fs}")
 
+    if sig.shape[0] != 2:
+        print("Necessary to use a stereo file")
+        exit()
+
     abs_spectrogram = signal2Spectrogram(sig)
     print("Spectrogram created")
 
-    secretBits = abs_spectrogram[1000]
-    print(f"Read {len(secretBits)} bits")
-    secretBits = unexpandBits(secretBits)
-    print(f"Bits unexpanded to {len(secretBits)} bits")
+    for fr in frequencies:
+        secretBits = abs_spectrogram[0][fr], invertBits(abs_spectrogram[1][fr])
+        print(f"Read {len(secretBits[0]), len(secretBits[1])} bits")
+
+        secretBits = checkAndUnexpandBits(secretBits)
+        print(f"Bits unexpanded to {len(secretBits)} bits")
+
+        # TODO: Calculate average or append each frequency
+
+    # print(secretBits[:8])
+
     secretBits = secretBits[:-(len(secretBits) % 8)]
 
+    lengthOfFilename = bits2Length(  secretBits[                                                                     : (BYTESFORLENGTHOFFILENAME                                                       )*8], BYTESFORLENGTHOFFILENAME)
     filename         = bits2Filename(secretBits[(BYTESFORLENGTHOFFILENAME                                        )*8 : (BYTESFORLENGTHOFFILENAME+lengthOfFilename                                      )*8])
-    lengthOfFilename = bits2Length(  secretBits[                                                                     : (BYTESFORLENGTHOFFILENAME                                                       )*8])
-    lengthOfSecret   = bits2Length(  secretBits[(BYTESFORLENGTHOFFILENAME+lengthOfFilename                       )*8 : (BYTESFORLENGTHOFFILENAME+lengthOfFilename+BYTESFORLENGTHOFSECRET               )*8])
+    lengthOfSecret   = bits2Length(  secretBits[(BYTESFORLENGTHOFFILENAME+lengthOfFilename                       )*8 : (BYTESFORLENGTHOFFILENAME+lengthOfFilename+BYTESFORLENGTHOFSECRET               )*8], BYTESFORLENGTHOFSECRET)
     secretBits       =               secretBits[(BYTESFORLENGTHOFFILENAME+lengthOfFilename+BYTESFORLENGTHOFSECRET)*8 : (BYTESFORLENGTHOFFILENAME+lengthOfFilename+BYTESFORLENGTHOFSECRET+lengthOfSecret)*8]
 
     print(f"Length of filename: {lengthOfFilename}")
@@ -220,6 +270,10 @@ def decode(audioFilename):
 
 
 if __name__ == "__main__":
+
+    # TODO:
+    frequencies = [1000,]
+
     code = input("Choose between encode or decode: ")
     if code == "encode":
         audioFile = input("Filename of the audio cover file: ")
